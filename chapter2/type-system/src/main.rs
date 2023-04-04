39fe2f7cdd392dca96f904498fb0024c2e12bed3
KazumasaYasui <silver.soul39@gmail.com>
2023-04-04 23:44:06 +0900

    整数型

--- chapter2/type-system/src/main.rs
+++ chapter2/type-system/src/main.rs
@@ -20,6 +20,30 @@ fn main() {
 
     println!("非短絡評価");
     println!("{}", a() | b());
+
+    // 加減乗除
+    let addition: u64 = 1234 + 567;
+    let subtraction: u64 = 678 - 168;
+    let multiplication: u64 = 56 * 146;
+    let division: u64 = 572 / 43;
+    let remainder: u64 = 145 % 23;
+    println!("1234 + 567 = {}", addition);
+    println!("678 - 168  = {}", subtraction);
+    println!("56 * 146   = {}", multiplication);
+    println!("572 / 43   = {}", division);
+    println!("145 % 23   = {}", remainder);
+
+    // 比較
+    let lt: bool = 1234 < 567;
+    let lteq: bool = 678 <= 168;
+    let gt: bool = 56 > 146;
+    let gteq: bool = 572 >= 43;
+    let eq: bool = 145 == 23;
+    println!("1234 < 567 = {}", lt);
+    println!("678 <= 168 = {}", lteq);
+    println!("56 > 146   = {}", gt);
+    println!("572 >= 43  = {}", gteq);
+    println!("145 == 23  = {}", eq);
 }
 
 fn a() -> bool {
